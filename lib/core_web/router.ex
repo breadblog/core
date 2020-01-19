c926405a32bb3f383afb468ead042a52469aebe2
parasrah <git@parasrah.com>
2020-01-18 20:51:17 -0700

    removing all controllers

--- lib/core_web/router.ex
+++ lib/core_web/router.ex
@@ -7,12 +7,5 @@ defmodule CoreWeb.Router do
 
   scope "/api", CoreWeb do
     pipe_through :api
-
-    resources "/post", PostController, except: [:new, :edit]
-    resources "/user", UserController, except: [:new, :edit]
-    resources "/author", AuthorController, except: [:new, :edit]
-    resources "/tag", TagController, except: [:new, :edit]
-    resources "/comment", CommentController, except: [:new, :edit]
-    resources "/error", ErrorController, except: [:new, :edit]
   end
 end
