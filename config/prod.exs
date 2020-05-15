f430852cc57268a8143b2878e0701790584fa436
parasrah <git@parasrah.com>
2020-05-14 19:19:49 -0600

    removing prod secrets

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,23 @@ config :blog_core, BlogCoreWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
+config :blog_core, BlogCoreWeb.Endpoint,
+  force_ssl: [hsts: true],
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  version: Mix.Project.config[:version] # to bust cache during hot upgrades
+
+config :blog_core, BlogCore.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
 # Do not print debug messages in production
 config :logger, level: :info
 
@@ -49,7 +66,3 @@ config :logger, level: :info
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
-
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
-import_config "prod.secret.exs"
