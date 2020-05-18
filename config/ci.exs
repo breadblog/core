2c818bb0c5d3977c4416f7f95b1465ad575dda80
parasrah <git@parasrah.com>
2020-05-17 18:10:52 -0600

    fixing connection string (hopefully)

--- config/ci.exs
+++ config/ci.exs
@@ -1,10 +1,25 @@
 use Mix.Config
 
+database_host = System.get_env("POSTGRES_HOST") ||
+  raise """
+  missing POSTGRES_HOST
+  """
+
+database_user = System.get_env("POSTGRES_USER") ||
+  raise """
+  missing POSTGRES_USER
+  """
+
+database_password = System.get_env("POSTGRES_PASSWORD") ||
+  raise """
+  missing POSTGRES_PASSWORD
+  """
+
 config :core, Core.Repo,
-  username: "postgres",
-  password: "postgres",
+  hostname: database_host,
+  user: database_user,
+  password: database_password,
   database: "core_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :core, CoreWeb.Endpoint,
