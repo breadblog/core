63578195695dfc3f05262bd79b239761004fe0cc
parasrah <git@parasrah.com>
2020-05-23 23:38:56 -0600

    mix format

--- config/prod.exs
+++ config/prod.exs
@@ -13,12 +13,15 @@ use Mix.Config
 config :core, CoreWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   load_from_system_env: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  server: true, # Without this line, your app will not start the web server!
+  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: {:system, "PORT"}],
+  # Without this line, your app will not start the web server!
+  server: true,
   secret_key_base: "${SECRET_KEY_BASE}",
   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  version: Mix.Project.config[:version] # to bust cache during hot upgrades
+  # to bust cache during hot upgrades
+  version: Mix.Project.config()[:version]
 
 config :logger, level: :info
 
@@ -27,7 +30,8 @@ config :core, Core.Repo,
   url: "${DATABASE_URL}",
   database: "",
   ssl: true,
-  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
 
 # ## SSL Support
 #
